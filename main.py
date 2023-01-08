074f857674dab9aad52656ca4fddab7a6bc769ad
elioverhoef <49610595+elioverhoef@users.noreply.github.com>
2023-01-08 10:49:36 +0100

    Pause audio button

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ passed = 0
 paused = False
 duration = 50 * 60
 text_area = pomo_count = False
+playing = False
 
 
 def start():
-    global timer, paused, passed
+    global timer, paused, passed, playing
 
-    winsound.PlaySound('40hz.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
+    start_playing()
     if paused:
         timer = time.perf_counter() - passed
         paused = False
@@ -28,9 +29,8 @@ def start():
 
 
 def pause():
-    global paused
-
-    winsound.PlaySound(None, winsound.SND_PURGE)
+    global paused, playing
+    stop_playing()
     paused = True
 
 
@@ -38,7 +38,7 @@ def stop():
     global paused, timer
     counter = get_counter()
 
-    winsound.PlaySound(None, winsound.SND_PURGE)
+    stop_playing()
     timer = time.perf_counter()
     increment_counter(counter)
     refresh()
@@ -61,6 +61,7 @@ def draw():
 
     pomo_count = CTkLabel(master=root, text=f"Pomos: {counter[str(date.today())]}",
                           font=("Arial", 18), text_color="darkgrey", height=10, bg_color="darkgreen")
+    mute_sound = CTkLabel(master=root, text="🔇", font=("Arial", 25), bg_color="darkgreen")
 
     # place widgets into window container using a layout
     start_button.grid(row=0, column=0)
@@ -68,6 +69,30 @@ def draw():
     stop_button.grid(row=0, column=2)
     text_area.grid(row=3, column=0, columnspan=3, rowspan=3)
     pomo_count.grid(row=5, column=2, rowspan=3)
+    mute_sound.grid(row=5, column=0, rowspan=3)
+    mute_sound.bind("<Button-1>", lambda _: switch_playing())
+
+
+def start_playing():
+    global playing
+    playing = True
+    winsound.PlaySound('40hz.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
+
+
+def stop_playing():
+    global playing
+    playing = False
+    winsound.PlaySound(None, winsound.SND_PURGE)
+
+
+def switch_playing():
+    global playing
+    if playing:
+        playing = False
+        winsound.PlaySound(None, winsound.SND_PURGE)
+    else:
+        playing = True
+        winsound.PlaySound('40hz.wav', winsound.SND_LOOP + winsound.SND_ASYNC)
 
 
 def refresh():
@@ -82,8 +107,8 @@ def refresh():
         seconds = "{:02d}".format(total_seconds - (total_seconds // 60) * 60)
         text_area.configure(text=f"{minutes}:{seconds}")
         pomo_count.configure(text=f"Pomos: {counter[str(date.today())]}")
-        if minutes == seconds == "00" or int(minutes) < 0:
-            winsound.PlaySound(None, winsound.SND_PURGE)
+        if minutes == seconds == "00" or total_seconds < 0:
+            stop_playing()
             winsound.PlaySound('done.wav', winsound.SND_ASYNC)
             increment_counter(counter)
             return
